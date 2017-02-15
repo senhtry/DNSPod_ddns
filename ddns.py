f002faae2614f08ee517be63be6cf466145a4ad5
senhtry <edleweiss@gmail.com>
2017-02-15 20:07:04 +0800

    modify string formater

--- ddns.py
+++ ddns.py
@@ -20,17 +20,17 @@ class Record:
         self.NextRunTime = datetime.datetime.now()
 
     def __str__(self):
-        print("%s.%s" % (self.SubDomain, self.Domain))
+        print("{!s}.{!s}".format(self.SubDomain, self.Domain))
 
     def GetRecordIP(self):
-        recordDomian = "%s.%s" % (self.SubDomain, self.Domain)
+        recordDomian = "{!s}.{!s}".format(self.SubDomain, self.Domain)
         res = requests.get("http://119.29.29.29/d", params={'dn': recordDomian}, timeout=20)
         return res.text.rstrip()
 
     def SetDDNS(self, localIP):
         self.Data.update(
             dict(
-                login_token=("%s,%s" % (self.ID, self.Token)),
+                login_token=("{!s},{!s}".format(self.ID, self.Token)),
                 format=self.Format,
                 domain_id=self.DomainID,
                 record_id=self.RecordID,
@@ -45,7 +45,7 @@ class Record:
                         "https://dnsapi.cn/Record.Ddns", headers=self.Headers, data=self.Data, timeout=20)
                     statusCode = res.json()["status"]["code"]
                     if statusCode == "1":
-                        self.PrintLog("DDNS updated to new ip: %s" % localIP)
+                        self.PrintLog("DDNS updated to new ip: {!s}".format(localIP))
                         curIP = localIP
                         self.NextRunTime = datetime.datetime.now() + datetime.timedelta(seconds=60)
                     elif statusCode == "-2":
@@ -53,17 +53,17 @@ class Record:
                         self.NextRunTime = datetime.datetime.now() + datetime.timedelta(hours=1)
                     else:
                         self.NextRunTime = datetime.datetime.now() + datetime.timedelta(seconds=60)
-                        raise Exception("DNSPod return error status code: %s" % statusCode)
+                        raise Exception("DNSPod return error status code: {!s}".format(statusCode))
                 else:
-                    self.PrintLog("DDNS already up-to-date: %s" % localIP)
+                    self.PrintLog("DDNS already up-to-date: {!s}".format(localIP))
                     self.NextRunTime = datetime.datetime.now() + datetime.timedelta(seconds=60)
         except Exception as ex:
             self.PrintLog(ex)
             self.NextRunTime = datetime.datetime.now() + datetime.timedelta(seconds=60)
 
     def PrintLog(self, msg):
-        print("Time: %s , Record: %s.%s : %s" %
-              (time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()), self.SubDomain, self.Domain, msg))
+        print("Time: {!s} , Record: {!s}.{!s} : {!s}".format(
+            time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()), self.SubDomain, self.Domain, msg))
 
 
 def GetLocalIP():
@@ -75,7 +75,7 @@ def LoadConfig(fileName):
     config = configparser.ConfigParser()
     fileList = config.read(fileName, encoding="utf8")
     if fileList:
-        print("Config file loaded: %s" % fileList)
+        print("Config file loaded: {!s}".format(fileList))
         records = []
         for section in config.sections():
             subDomain = config[section]
@@ -89,7 +89,7 @@ def LoadConfig(fileName):
             record.SubDomain = subDomain.get("SubDomain")
             record.RecordLine = subDomain.get("RecordLine")
             records.append(record)
-            record.PrintLog("Config loaded : %s.%s" % (record.SubDomain, record.Domain))
+            record.PrintLog("Config loaded : {!s}.{!s}".format(record.SubDomain, record.Domain))
         return records
     else:
         print("Cannot found config file")
